5ef80b0f364d280369a2c788bbbab001564bd885
Lucasema <lucasema21@gmail.com>
2023-06-10 21:39:58 -0300

    agrego archivos

---
+++ test_main.py
@@ -0,0 +1,13 @@
+import unittest
+import main
+
+
+class TestClass(unittest.TestCase):
+
+    def test_add(self):
+        result = main.add(10, 5)
+        self.assertEquals(result, 15)
+
+
+if __name__ == '__main__':
+    unittest.main()
